05a603822c5f4a4cceeeda5a6143284bb2c1170b
SameerKhan-Dev <sameerk_786@hotmail.com>
2021-03-05 06:15:59 +0000

    added REST endpoints as comments

--- server.js
+++ server.js
@@ -13,12 +13,93 @@ const morgan = require("morgan");
 const cookieSession = require("cookie-session");
 const database = require("./database/database");
 var path = require('path');
-
+const bcrypt = require('bcrypt');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/*
+    * Have correct formatting
 
+* Have resilient error handling
+
+* Exceptions should appropriate handling
+
+* Architecture should be scalable, easy to maintain
+
+* Tricky parts of the code should have proper documentation
+
+** Database queries should be efficient
+
+
+
+Section 1
+
+
+
+* User registration using unique username and a password
+
+* User login (Including session maintenance using any means you're comfortable with)
+
+* Unit tests for these basic methods
+
+
+
+These two APIs must be perfect. DO NOT move on to the remainder of the assignment until these are completed. If either of these APIs are missing or incomplete, the remainder of the assignment WILL NOT be scored at all.
+
+
+
+Section 2
+
+
+
+Start *only* once the Basic Functionality is complete. Complete these *in the order specified*
+
+
+
+* Chat with other users
+
+* Create, read, update, delete tweet (Twitter doesn't support update, can you?)
+
+* Unit/Integration tests for *all* endpoints you've built so far (Basic & Extended Functionality)
+REGISTRATION:
+
+Create new user
+POST  /users/new
+
+Login
+POST /login
+
+Logout
+POST /logout
+
+TWEETS:
+Create Tweet:
+POST /tweets
+
+Update Tweets:
+PUT /tweets/:id
+
+Delete tweet:
+DELETE /tweets/:id
+
+GET A SPECFIC TWEET:
+GET /tweets/:id
+
+GET All tweets for a User:
+GET /users/:id/tweets
+
+GET ALL liked tweets for a user:
+GET /users/:id/likedTweets
+
+MESSAGES:
+GET All conversations for a user
+/users/:id/conversations
+
+GET All messages for a specific conversation
+/conversations/:id/messages
+
+*/
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
@@ -47,5 +128,8 @@ app.get("/hello", (req,res) => {
 
     res.send("Hello There!");
 });
-
+/*
+Create new user
+POST  /users/new
+*/
 module.exports = app;
