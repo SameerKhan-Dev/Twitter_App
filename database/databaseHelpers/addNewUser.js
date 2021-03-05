73dfd62f8ff7dd5abeb212001ead0f21d1d331ab
SameerKhan-Dev <sameerk_786@hotmail.com>
2021-03-05 06:27:52 +0000

    implemented database helper function addNewUser for registration

---
+++ database/databaseHelpers/addNewUser.js
@@ -0,0 +1,23 @@
+const db = require("../database");
+
+const addNewUser = function (userName, email, password) {
+  return db
+    .query(
+      `
+      INSERT INTO users (userName, email, password)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *;`, [userName, email, password]
+    )
+    .then((res) => {
+      if (res.rows) {
+        console.log("res.rows is, ", res.rows);
+        return res.rows;
+      } else {
+        console.log("null returned");
+        return null;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = addNewUser;
