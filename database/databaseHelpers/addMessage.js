f9a9650a18addbb8519e8302344691736985850b
SameerKhan-Dev <sameerk_786@hotmail.com>
2021-03-06 06:49:07 +0000

    implemented database helper functions for adding message and for getting all messages for a conversation

---
+++ database/databaseHelpers/addMessage.js
@@ -0,0 +1,35 @@
+const db = require("../database");
+
+const addMessage = function (conversation_id, sender_id, receiver_id, description){
+  return db
+    .query(
+      `
+      INSERT INTO messages (conversation_id, sender_id, receiver_id, description)
+      VALUES ($1, $2, $3)
+      RETURNING *;`, [conversation_id, sender_id, receiver_id, description]
+    )
+    .then((res) => {
+      if (res.rows) {
+        //console.log("res.rows is, ", res.rows);
+        return res.rows;
+      } else {
+        console.log("null returned");
+        return null;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = addMessage;
+
+/* messages Table 
+DROP TABLE IF EXISTS messages CASCADE;
+CREATE TABLE messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  conversation_id INTEGER REFERENCES conversations(id) ON DELETE CASCADE,
+  sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  receiver_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+*/
---
+++ database/databaseHelpers/getAllMessagesForConversation.js
@@ -0,0 +1,23 @@
+const db = require("../database");
+
+const getAllMessagesForConversation = function (conversation_id){
+  return db
+    .query(
+      `
+      SELECT * FROM messages
+      WHERE conversation_id = $1
+      ;`, [conversation_id]
+    )
+    .then((res) => {
+      if (res.rows) {
+        //console.log("res.rows is, ", res.rows);
+        return res.rows;
+      } else {
+        console.log("null returned");
+        return null;
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+module.exports = addMessage;
